ff1bdd5375aa8df8f8957b9d41c1039a1275daff
Mikey <spannmikey@gmail.com>
2023-08-24 21:18:27 -0500

    staring styling and declaring variables and functions

---
+++ build/app.js
@@ -0,0 +1,16 @@
+const num = document.querySelector(".num");
+const increment = document.querySelector(".btn-increase");
+const decrement = document.querySelector(".btn-decrease");
+const reset = document.querySelector(".btn-reset");
+
+// Global function that runs everything
+function run() {}
+
+// Increment values
+function increase() {}
+
+// Decrement values
+function decrease() {}
+
+// Reset values
+function reset() {}
